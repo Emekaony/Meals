9a7de0cd2823f811960e99d05090ff70f1653086
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-11-20 17:52:21 -0800

    Trying to fix library compatibility issues

--- App.js
+++ App.js
@@ -2,13 +2,25 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "./Screens/CategoriesScreen";
 import MealsOverviewScreen from "./Screens/MealsOverviewScreen";
 import MealDetailScreen from "./Screens/MealDetailScreen";
+import FavoritesScreen from "./Screens/FavoritesScreen";
 
 // this is an object
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Categories" component={CategoriesScreen} />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+};
 
 export default function App() {
   return (
@@ -27,22 +39,13 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="CategoriesScreen"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
               title: "All Categories",
             }}
           />
-          <Stack.Screen
-            name="OverviewScreen"
-            component={MealsOverviewScreen}
-            // options={({ route, navigation }) => {
-            //   const catId = route.params.categoryId;
-            //   return {
-            //     title: catId,
-            //   };
-            // }}
-          />
+          <Stack.Screen name="OverviewScreen" component={MealsOverviewScreen} />
           <Stack.Screen name="DetailScreen" component={MealDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
